6eff8004d13c14f909cea3263bd9345aeb96a4b3
miko.fugo <okuramarusaikou@outlook.jp>
2020-01-07 00:01:23 +0900

    refs #1 envファイル追加、ルートディレクトリ修正、docker-compose.ymlファイルディレクトリ修正、buildが通らない

--- docker-compose/start_app.sh
+++
@@ -1,8 +0,0 @@
-# MySQLサーバーが起動するまでmain.goを実行せずにループする
-until mysqladmin ping -h mysql --silent; do
-    echo 'wating for mysqld to be connectable...'
-    sleep 2
-done
-
-echo "app is starting...!"
-exec go run main.go
---
+++ src/start_app.sh
@@ -0,0 +1,6 @@
+# MySQLサーバーが起動するまでmain.goを実行せずにループする
+until mysqladmin ping -h mysql -P 3306 --silent;
+do echo 'waiting for mysqld to be connectable...'
+sleep 2
+done echo 'app is starting...!'
+exec go run main.go
